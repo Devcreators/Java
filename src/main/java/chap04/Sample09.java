e8340d195281266fad1a8c052b5b85a1b3d7cd8d
Devcreators <a41417144@gmail.com>
2021-02-08 16:06:13 +0900

    commit : ch04 연산자 빵형의 실전 자바 : 예제01~16
    커밋번호 03

---
+++ src/main/java/chap04/Sample09.java
@@ -0,0 +1,16 @@
+package chap04;
+
+public class Sample09 {
+
+	public static void main(String[] args) {
+		int a = 60;
+		int b = -10;
+		
+		System.out.println("* OR");
+		System.out.printf("[%32s] %d\n", Integer.toBinaryString(a), a);
+		System.out.printf("[%32s] %d\n", Integer.toBinaryString(b), b);
+		System.out.println("=========================================");
+		System.out.printf("[%32s] %d\n", Integer.toBinaryString(a | b), a | b);
+	}
+
+}
---
+++ src/main/java/chap04/Sample11.java
@@ -0,0 +1,16 @@
+package chap04;
+
+public class Sample11 {
+
+	public static void main(String[] args) {
+		int a = 60;
+		int b = -10;
+		
+		System.out.println("* NOT");
+		System.out.printf("[%32s] ~ [%32s] = %d ~ %d\n", 
+				Integer.toBinaryString(a), Integer.toBinaryString(~a), a, ~a);
+		System.out.printf("[%32s] ~ [%32s] = %d ~ %d\n", 
+				Integer.toBinaryString(b), Integer.toBinaryString(~b), b, ~b);
+	}
+
+}
