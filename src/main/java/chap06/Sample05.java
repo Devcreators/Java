72fbc898d923f76d6375d53e0b56f8d52a0421fc
Devcreators <a41417144@gmail.com>
2021-02-10 19:25:24 +0900

    commit : ch06 반복문 빵형의 실전 자바 : 예제01~11
    커밋번호 05

---
+++ src/main/java/chap06/Sample05.java
@@ -0,0 +1,18 @@
+package chap06;
+
+public class Sample05 {
+
+	public static void main(String[] args) {
+		// 초기식과 증감식이 1개만 오는게 아니다.
+		for (int i = 0, j = 9; i < 5; i++, j -= 2) {
+			for (int k = 0; k < i; k++) {
+				System.out.printf(" ");
+			}
+			for (int h = 0; h < j; h++) {
+				System.out.printf("#");
+			}
+			System.out.println();
+		}
+	}
+
+}
