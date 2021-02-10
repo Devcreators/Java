0eb49cd93d2e907cd09aafaa609f8dd5c1c152d3
Devcreators <a41417144@gmail.com>
2021-02-10 21:02:40 +0900

    commit : ch07 조건문 빵형의 실전 자바 : 예제01~11
    커밋번호 06

---
+++ src/main/java/chap07/Sample07.java
@@ -0,0 +1,21 @@
+package chap07;
+
+public class Sample07 {
+	public static void main(String[] args) { 
+		// Java 12 버전에서 미리보기 기능으로 지원
+		// Java 12 이후 버전에서는 사용 불가
+		int in = 21;
+
+//		switch(in) {
+//			case 1, 3, 5, 7, 9:
+//				System.out.println("홀수입니다.");
+//				break;
+//			case 2, 4, 6, 8, 10:
+//				System.out.println("짝수입니다.");
+//				break;
+//			default:
+//				System.out.println("1부터 10까지의 수가 아닙니다.");
+//		}
+		
+	}
+}
---
+++ src/main/java/chap07/Sample10.java
@@ -0,0 +1,18 @@
+package chap07;
+
+public class Sample10 {
+
+	public static void main(String[] args) {
+		int in = 6; // 입력 월
+// Java 12 버전부터는 향상된 switch문에서 break문으로 값을 반환할 수 없습니다.	
+//		String season = switch (in) {
+//			case 12, 1, 2 : break "겨울";
+//			case 3, 4, 5 : break "봄";
+//			case 6, 7, 8 : break "여름";
+//			case 9, 10, 11 : break "가을";
+//			default : break "기후온난화";		
+//		};
+//
+//		System.out.println(season);
+	}
+}
