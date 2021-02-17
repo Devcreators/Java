f901a4de4052f6f1e6d39213b4ade7bf968029ca
Devcreators <a41417144@gmail.com>
2021-02-17 20:52:24 +0900

    commit : ch11 컬렉션 프레임워크 빵형의 실전 자바 : 예제01~13
    커밋번호 10

---
+++ src/main/java/chap11/Sample08.java
@@ -0,0 +1,29 @@
+package chap11;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Sample08 {
+
+	public static void main(String[] args) {
+		var list = new ArrayList<String>();
+		list.add("dog");
+		list.add("cat");
+		list.add("lion");
+		System.out.println(list);
+		System.out.printf("총 %d의 자료가 있습니다.%n%n", list.size());
+		
+		Iterator<String> it = list.iterator();
+		while(it.hasNext()) {
+			String item = it.next();
+			System.out.println(item);
+			if("cat".equals(item)) {
+				it.remove();
+			}
+		}
+
+		System.out.println("\n" + list);
+		System.out.printf("총 %d의 자료가 있습니다.%n%n", list.size());
+		
+	}
+}
